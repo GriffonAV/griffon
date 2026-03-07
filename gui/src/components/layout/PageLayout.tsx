7f754f5f056ae0eed04174a060b4b6df5196e062
Diabolo <alexis.boitel@proton.me>
2026-03-07 15:30:14 +0900

    feat(gui/layout): dynamic icon for dynamic plugin ui

--- gui/src/components/layout/PageLayout.tsx
+++ gui/src/components/layout/PageLayout.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import { Button } from "../ui/button";
 import { PanelLeft } from "lucide-react";
 
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Separator } from "@/components/ui/separator";
+
 interface PageProps {
   title?: string;
   children?: React.ReactNode;
+  navigation?: Boolean;
+  sections?: string[];
 }
 
-export const PageWrapper: React.FC<PageProps> = ({ title, children }) => {
+export const PageWrapper: React.FC<PageProps> = ({ title, children, navigation, sections }) => {
   return (
     <div className="bg-background text-foreground flex-1 flex-col m-2 rounded-md overflow-hidden flex">
       <div className="flex items-center border-b rounded-none p-2 gap-4">
@@ -22,9 +33,31 @@ export const PageWrapper: React.FC<PageProps> = ({ title, children }) => {
         <div className="border-r rounded-none w-0 h-6"></div>
         {title && <h1>{title}</h1>}
       </div>
+      {
+        navigation &&
+        <div className="flex p-2 shadow">
+          <NavigationMenu className="h-min">
+            <NavigationMenuList>
+              {/* <NavigationMenuItem>
+                <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                  <span>Appearance</span>
+                </NavigationMenuLink>
+              </NavigationMenuItem> */}
+              {sections?.map((section) => (
+                <NavigationMenuItem >
+                  <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                    <span>{section} </span>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              ))
+              }
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      }
       <main className="flex flex-col m-2 flex-1 overflow-auto sm:px-0 md:px-7 lg:px-36 items-start gap-4">
         {children}
       </main>
-    </div>
+    </div >
   );
 };
--- gui/src/pages/SettingsPage.tsx
+++ gui/src/pages/SettingsPage.tsx
@@ -1,33 +1,10 @@
 import { ChangeThemeButtonTest, ModeToggleGroup } from "@/components/layout/ModeToggle";
 import { PageWrapper } from "@/components/layout/PageLayout";
 
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
 
 export default function SettingsPage() {
   return (
-    <PageWrapper title="Settings">
-      <div className="flex">
-        <NavigationMenu className="h-min">
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                <span>Appearance</span>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                <span>Notifications</span>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
+    <PageWrapper title="Settings" navigation={true} sections={["Appearance", "Notifications"]}>
       <div>
         <h2 className="text-xl font-bold">Appearance</h2>
         <div className="flex flex-col m-5">
@@ -41,6 +18,10 @@ export default function SettingsPage() {
         <h2 className="text-xl font-bold p-2">Notifications</h2>
         <p></p>
       </div>
-    </PageWrapper>
+      lorem ipsum, lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+      lorem ipsum, lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+      lorem ipsum, lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+
+    </PageWrapper >
   );
 }
