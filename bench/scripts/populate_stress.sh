4ac113f1ea4869c5907226cad8b756c6aabfaeaa
ewen1507 <ewen.emeraud@gmail.com>
2026-04-01 09:47:41 +0900

    feat(populate_stress.sh): increase stress test file generation to 20,000 files

--- bench/scripts/populate_stress.sh
+++ bench/scripts/populate_stress.sh
@@ -7,7 +7,7 @@ echo "Generating STRESS dataset..."
 mkdir -p /tmp/griffon_stress
 mkdir -p ~/.cache/griffon_stress
 
-for i in {1..2000}
+for i in {1..20000}
 do
     head -c 100K </dev/urandom > /tmp/griffon_stress/file_$i.tmp
 done
