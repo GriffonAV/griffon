4d1dccd13feb405833382dae9a9671579c8528b1
Diabolo <alexis.boitel@proton.me>
2026-09-07 16:54:57 +0200

    chore: Release

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 mkdir -p /out
-nfpm pkg --packager deb --target /out/griffon-0.3.deb
-nfpm pkg --packager rpm --target /out/griffon-0.3.rpm
+nfpm pkg --packager deb --target /out/griffon-0.3.1.deb
+nfpm pkg --packager rpm --target /out/griffon-0.3.1.rpm
